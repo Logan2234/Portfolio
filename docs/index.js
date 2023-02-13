38d21234866c587d8cfc34bdea57c89190a52d9c
Logan Willem <logan.willem@grenoble-inp.org>
2023-02-13 21:28:46 +0100

    Better header v0.7.1

--- docs/index.js
+++ docs/index.js
@@ -13,7 +13,7 @@ function scrollFunc() {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrolled = (winScroll / height) * 100;
-    if (winScroll > 200) {
+    if (winScroll > 700) {
         document.getElementsByTagName("header")[0].classList.add("scrolled");
     } else {
         document.getElementsByTagName("header")[0].classList.remove("scrolled");
